28197bc131da653585cdeaa4a437ba12b9f6777b
jeanmfdias <jeanmfdias@gmail.com>
2018-02-25 09:02:10 -0300

    Initial files to test with mocha

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('ParserGameLog', function() {
+  describe('loadLog', function() {
+    it('load file', function() {
+      assert.equal(1, 1)
+    })
+  })
+})
