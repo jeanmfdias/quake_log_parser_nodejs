54cb2efa41e2d764d7bb3c0a441bf6c0512eed65
jeanmfdias <jeanmfdias@gmail.com>
2018-03-01 21:27:31 -0300

    Added module chai for assert test, and describe test for loadLog

--- test/test.js
+++ test/test.js
@@ -1,28 +1,25 @@
-const assert = require('assert');
-const ParserGameLog = require('../app/modules/ParserGameLog')
-
-describe('ParserGameLog', function() {
-  describe('loadLog', function() {
-    it('if var is a object', function() {
-      var pgl = new ParserGameLog('games.log')
-      assert.equal(typeof pgl, 'object')
-    })
-  })
-})
-
-describe('ParserGameLog', function() {
-  describe('loadLog', function() {
-    it('if load file', function() {
-      var pgl = new ParserGameLog('games.log')
-    })
-  })
-})
+const assert = require('chai').assert;
+const ParserGameLog = require('../app/modules/ParserGameLog');
 
 describe('ParserGameLog', function() {
   describe('loadLog', function() {
     it('if don\'t load file', function() {
-      var pgl = new ParserGameLog()
-      assert.equal(pgl.getFileLine())
-    })
-  })
-})
+      var pgl = new ParserGameLog();
+
+      assert.equal(pgl.getFileLine());
+    });
+
+    it('if var is a object', function() {
+      var pgl = new ParserGameLog('games.log');
+
+      assert.isObject(pgl);
+    });
+
+    it('if load file', function() {
+      var pgl = new ParserGameLog('games.log');
+
+      assert.notEqual(pgl.getFileLine());
+    });
+  });
+
+});
